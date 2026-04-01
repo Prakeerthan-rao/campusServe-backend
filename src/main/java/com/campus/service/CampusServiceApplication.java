2e54d62230083768340fa421c4a8db43ec5bf15e
P.Prakeerthan Rao <prakeerthanrao15@gmail.com>
2026-04-01 15:50:57 +0530

    Update CampusServiceApplication.java

--- src/main/java/com/campus/service/CampusServiceApplication.java
+++ src/main/java/com/campus/service/CampusServiceApplication.java
@@ -1,4 +1,5 @@
 package com.campus.service;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
