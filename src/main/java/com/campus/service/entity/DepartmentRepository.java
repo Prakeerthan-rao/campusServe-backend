783be01447232295650d63c1618c14fdd19b0be1
P.Prakeerthan Rao <prakeerthanrao15@gmail.com>
2026-04-01 09:53:16 +0530

    Create DepartmentRepository.java

---
+++ src/main/java/com/campus/service/entity/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.campus.service.repository;
+
+import com.campus.service.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
